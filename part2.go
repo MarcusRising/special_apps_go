49c275185ad18a3f202ce7b33b3fc5af598eaec8
MarcusRising <129616087+MarcusRising@users.noreply.github.com>
2024-02-05 09:12:21 -0700

    Add files via upload

---
+++ part2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func new() {
+// 	const name, age = "Mark", 35
+// 	fmt.Printf("%s is %d years old.\n", name, age)
+// 	//fmt.Println(say)
+// 	fmt.Printf("%v is %v years old.\n and the type is %T and %T\n", name, age, name, age) //v gives the default   T is for type
+// 	fmt.Println(`This
+// 	is
+// 	a
+// 	test
+// 	of
+// 	things 💥`) //raw string
+// }
+
+// func handsOn1() {
+// 	const crazy = "💥🔽✨✔🎉🔼🛑"
+// 	fmt.Printf("%v\n", crazy)
+// 	fmt.Println(
+// 		`W
+// 	O
+// 	R
+// 	D!`)
+
+// }
+
+func main() {
+	//handsOn1()
+
+	if z := 80; z > 50 {
+		fmt.Println(z)
+	}
+}
